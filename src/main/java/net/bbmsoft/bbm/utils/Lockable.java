40081a513cd11f21f5e7fe42469191a4eb2dfe6b
Michael Bachmann <kontakt@babymotte.de>
2018-03-24 13:33:49 +0100

    added javadoc to Lockable

--- src/main/java/net/bbmsoft/bbm/utils/Lockable.java
+++ src/main/java/net/bbmsoft/bbm/utils/Lockable.java
@@ -1,9 +1,24 @@
 package net.bbmsoft.bbm.utils;
 
+/**
+ * Simple interface defining the capability to lock and unlock something.
+ * <p>
+ * This interface does not impose any requirements about what being locked or
+ * unlocked implies for the implementing object.
+ * 
+ * @author Michael Bachmann
+ *
+ */
 public interface Lockable {
 
+	/**
+	 * Locks this {@link Lockable}.
+	 */
 	public abstract void lock();
-	
+
+	/**
+	 * Unocks this {@link Lockable}.
+	 */
 	public abstract void unlock();
-	
+
 }
